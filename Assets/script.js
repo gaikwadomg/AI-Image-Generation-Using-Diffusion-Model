51054acf4597fef075dae003fb8c7a9366dc1f64
gaikwadomg <connect2ompc@gmail.com>
2025-02-20 02:01:55 +0530

    added files

---
+++ Assets/script.js
@@ -0,0 +1,33 @@
+const key = "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";
+const inputText = document.getElementById("input");
+const image = document.getElementById("image");
+const GenBtn = document.getElementById("btn");
+
+async function query(data) {
+	const response = await fetch(
+		"https://router.huggingface.co/hf-inference/models/ZB-Tech/Text-to-Image",
+		{
+			headers: {
+				Authorization: `Bearer ${key}`,
+				"Content-Type": "application/json",
+			},
+			method: "POST",
+			body: JSON.stringify({"inputs": inputText.value}),
+		}
+	);
+	const result = await response.blob();
+	return result;
+}
+
+async function generate(){
+    query().then((response) => {
+        // Use image
+        const objUrl = URL.createObjectURL(response);
+        image.src = objUrl;
+    });
+}
+
+GenBtn.addEventListener("click", () => {
+    alert("Please wait...");
+    generate();
+});
